fd7025e7ed115afb8b02785194a58e6696ca269a
ChenouWu <155577614+ChenouWu@users.noreply.github.com>
2025-08-31 14:21:47 -0300

    Update index.js
    Fixed Index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"https://aichatbox-93ux.onrender.com"],
     credentials: true,
 }));
 
